d3c4645a9c59dbf2f1435270abe960ab1d0ccd21
sahargsim <43333221+sahargsim@users.noreply.github.com>
2021-04-29 20:27:40 +0200

    Update product.controller.js

--- backend/app/controllers/product.controller.js
+++ backend/app/controllers/product.controller.js
@@ -87,7 +87,7 @@ exports.update = (req, res) => {
       });
   };
 
-// Delete a Tutorial with the specified id in the request
+// Delete a Product with the specified id in the request
 exports.delete = (req, res) => {
     const id = req.params.id;
   
